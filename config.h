1a68bfbd421379b360ef6c1c3d8602cc7781a9f8
HvalaVitalisu <hvalavitalisu@gmail.com>
2022-10-03 07:53:35 +0200

    added swallow patch

--- config.h
+++ config.h
@@ -8,6 +8,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 7, .gappx = 7 };
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "fontawesome:size=11", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
@@ -37,9 +38,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class        instance    title         tags mask     isfloating  isterminal   noswallow  monitor */
+	{ "Gimp",        NULL,       NULL,          0,            1,          	0,          0,       -1 },
+	{ "Firefox",     NULL,       NULL,          1 << 8,       0,           	0, 	   -1,       -1 },
+	{ "st-256color", NULL,       NULL,          0,            0,            1,          0,       -1 },
+	{  NULL,         NULL,      "Event Tester", 0,            0,            0,          1,       -1 },      
 };
 
 /* layout(s) */
@@ -82,8 +85,8 @@ static const char *mutevol[] = { "amixer", "-q", "set", "Master", "toggle", NULL
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ 0,                            XK_F6,     spawn,          {.v = (const char*[]){"./bright.sh", "+5", NULL } } },
-	{ 0,                            XK_F5,     spawn,          {.v = (const char*[]){"./bright.sh", "-5", NULL } } },
+	{ 0,                            XK_F6,     spawn,          {.v = (const char*[]){"sudo", "./bright.sh", "+5", NULL } } },
+	{ 0,                            XK_F5,     spawn,          {.v = (const char*[]){"sudo", "./bright.sh", "-5", NULL } } },
 	{ MODKEY,                       XK_a,      spawn,          {.v = (const char*[]){"st", "-e", "ani-cli", NULL } } }, //weeb
 	{ MODKEY,                       XK_s,      spawn,          {.v = (const char*[]){ "stremio", NULL } } },
 	{ MODKEY,                       XK_e,      spawn,          {.v = (const char*[]){TERM, "-e", "htop", NULL } } },
